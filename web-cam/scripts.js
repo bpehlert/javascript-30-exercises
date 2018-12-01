4001f9d1826f83e677535aaf74af6f61cc9176bf
Ben Ehlert <benjamin.ehlert@gmail.com>
2018-12-01 14:29:49 -0700

    Adding new web cam files, exercise finished.

--- web-cam/scripts.js
+++ web-cam/scripts.js
@@ -24,7 +24,20 @@ function paintToCanvas() {
 
   setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
-  }, 1);
+    //  Take the pixels out
+    let pixels = ctx.getImageData(0, 0, width, height);
+
+    //  //  change the pixels
+    //  pixels = redEffect(pixels);
+
+    //  pixels = rgbSplit(pixels);
+    //  ctx.globalAlpha = 0.1;
+
+    pixels = greenScreen(pixels);
+
+    //  Put them back
+    ctx.putImageData(pixels, 0, 0);
+  }, 16);
 }
 
 function takePhoto() {
@@ -39,6 +52,53 @@ function takePhoto() {
   link.setAttribute("download", "Web Cam App");
   link.innerHTML = `<img src="${data}" alt="photo" />`;
   strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i] = pixels.data[i + 0] + 150; //Red
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; //Green
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; //Blue
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 150] = pixels.data[i + 0]; //Red
+    pixels.data[i + 100] = pixels.data[i + 1]; //Green
+    pixels.data[i - 150] = pixels.data[i + 2]; //Blue
+  }
+  return pixels;
+}
+
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll(".rgb input").forEach(input => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (
+      red >= levels.rmin &&
+      green >= levels.gmin &&
+      blue >= levels.bmin &&
+      red <= levels.rmax &&
+      green <= levels.gmax &&
+      blue <= levels.bmax
+    ) {
+      // take it out!
+      pixels.data[i + 3] = 0;
+    }
+  }
+
+  return pixels;
 }
 
 getVideo();
