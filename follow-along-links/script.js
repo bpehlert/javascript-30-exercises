af6d06b7f2bcbe8e7333a451c12be148ac796ca5
Ben Ehlert <benjamin.ehlert@gmail.com>
2018-12-04 17:48:55 -0700

    new exercise completed

---
+++ follow-along-links/script.js
@@ -0,0 +1,23 @@
+const links = document.querySelectorAll("a");
+
+const highlight = document.createElement("span");
+highlight.classList.add("highlight");
+document.body.append(highlight);
+
+function highlightLink() {
+  const linkCoords = this.getBoundingClientRect();
+  //   console.log(linkCoords);
+
+  const coords = {
+    width: linkCoords.width,
+    height: linkCoords.height,
+    top: linkCoords.top + window.scrollY,
+    left: linkCoords.left + window.screenX
+  };
+
+  highlight.style.width = `${coords.width}px`;
+  highlight.style.height = `${coords.height}px`;
+  highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
+}
+
+links.forEach(a => a.addEventListener("mouseenter", highlightLink));
