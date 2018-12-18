4cc9f71fc36c4d4af2d81875d2ecb832dcddddb9
Ben Ehlert <benjamin.ehlert@gmail.com>
2018-12-17 17:50:25 -0700

    finished up video playback exercise

--- video-speed-controller/script.js
+++ video-speed-controller/script.js
@@ -2,11 +2,17 @@ const video = document.querySelector("video");
 const speed = document.querySelector(".speed-bar");
 const bar = document.querySelector(".speed");
 
-// console.log(video, speed, bar);
-
 function handleSpeedChange(e) {
   const y = e.pageY - this.offsetTop;
-  const 
+  const percent = y / this.offsetHeight;
+  const height = Math.round(percent * 100) + "%";
+  const rateMin = 0.4;
+  const rateMax = 4;
+  const rate = percent * (rateMax - rateMin) + rateMin;
+
+  speed.style.height = height;
+  speed.textContent = rate.toFixed(1) + "x";
+  video.playbackRate = rate;
 }
 
 bar.addEventListener("mousemove", handleSpeedChange);
